f35927aa764c77e5ef70689b89114e5a28c7cd00
Konstantin Bukin <bukin@inetstudio.ru>
2018-01-15 16:35:36 +0300

    Optimize fields

---
+++ resources/views/back/forms/fields/classifiers.blade.php
@@ -0,0 +1,17 @@
+@php
+    $item = $value;
+@endphp
+
+{!! Form::dropdown('classifiers[]', $item->classifiers()->where('type', $attributes['field']['type'])->pluck('classifiers.id')->toArray(), [
+    'label' => [
+        'title' => $attributes['label']['title'],
+    ],
+    'field' => [
+        'class' => 'select2 form-control',
+        'data-placeholder' => $attributes['field']['placeholder'],
+        'style' => 'width: 100%',
+        'multiple' => 'multiple',
+        'data-source' => route('back.classifiers.getSuggestions', ['type' => $attributes['field']['type']]),
+    ],
+    'options' => (old('classifiers')) ? \InetStudio\Classifiers\Models\ClassifierModel::whereIn('id', old('classifiers'))->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray() : $item->classifiers()->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray(),
+]) !!}
---
+++ src/Providers/FormBuilderServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace InetStudio\Classifiers\Providers;
+
+use Collective\Html\FormBuilder;
+use Illuminate\Support\ServiceProvider;
+
+class FormBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Загрузка сервиса.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        FormBuilder::component('classifiers', 'admin.module.classifiers::back.forms.fields.classifiers', ['name' => null, 'value' => null, 'attributes' => null]);
+    }
+
+    /**
+     * Регистрация привязки в контейнере.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
