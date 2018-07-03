bb37bf07524c2efafe3f15104423d3b809e43165
Konstantin Bukin <bukin@inetstudio.ru>
2018-07-03 03:30:58 +0300

    Fixes

--- resources/views/back/forms/fields/classifiers.blade.php
+++ resources/views/back/forms/fields/classifiers.blade.php
@@ -1,19 +1,27 @@
 @php
     $item = $value;
+
+    $values = $item->classifiers()->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray();
+
+    if (empty($values) && isset($attributes['field']['default'])) {
+        $values = \InetStudio\Classifiers\Models\ClassifierModel::where('type', $attributes['field']['type'])
+            ->whereIn('value', (array) $attributes['field']['default'])
+            ->pluck('classifiers.value', 'classifiers.id')
+            ->toArray();
+    }
 @endphp
 
-{!! Form::dropdown('classifiers[]', $item->classifiers()->where('type', $attributes['field']['type'])->pluck('classifiers.id')->toArray(), [
+{!! Form::dropdown('classifiers[]', array_keys($values), [
     'label' => [
         'title' => $attributes['label']['title'],
     ],
-    'field' => [
+    'field' => array_merge([
         'class' => 'select2 form-control',
         'data-placeholder' => $attributes['field']['placeholder'],
         'style' => 'width: 100%',
-        'multiple' => 'multiple',
         'data-source' => route('back.classifiers.getSuggestions', ['type' => $attributes['field']['type']]),
-    ],
+    ], ((! isset($attributes['field']['multiple'])) || isset($attributes['field']['multiple']) && $attributes['field']['multiple']) ? ['multiple' => 'multiple'] : []),
     'options' => [
-        'values' => (old('classifiers')) ? \InetStudio\Classifiers\Models\ClassifierModel::whereIn('id', old('classifiers'))->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray() : $item->classifiers()->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray(),
+        'values' => (old('classifiers')) ? \InetStudio\Classifiers\Models\ClassifierModel::whereIn('id', old('classifiers'))->where('type', $attributes['field']['type'])->pluck('classifiers.value', 'classifiers.id')->toArray() : $values,
     ],
 ]) !!}
